d2b8e7ffb6e6af4358e52f9ed63ad1c26b2b9297
aranair <boa.homan@gmail.com>
2017-11-20 15:58:25 -0500

    Config first pass

--- config/config.go
+++ config/config.go
@@ -1,23 +1,42 @@
 package config
 
+import (
+  "github.com/spf13/viper"
+  "fmt"
+)
+
 type Config struct {
-  Coins coin
+  Coins []coin
 }
 
 type coin struct {
-	Symbol     string `json:"symbol"`
-  Name     string `json:"name"`
-	cost string `json:"cost"`
+  Name string
+  Cost float64
+  Units float64
 }
 
 func LoadConfiguration(file string) Config {
-    var config Config
-    configFile, err := os.Open(file)
-    defer configFile.Close()
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-    jsonParser := json.NewDecoder(configFile)
-    jsonParser.Decode(&config)
-    return config
+  fmt.Println(".........")
+  viper.SetConfigType("yaml")
+  viper.SetConfigFile(file)
+
+  if err := viper.ReadInConfig(); err != nil {
+    fmt.Println("2")
+    // log.Fatalf("Error reading config file, %s", err)
+  }
+
+  fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+  var C Config
+  err := viper.Unmarshal(&C)
+  if err != nil {
+    panic(err)
+  }
+  // viper.WatchConfig()
+
+  // viper.OnConfigChange(func(e fsnotify.Event) {
+  //   fmt.Println("Config file changed:", e.Name)
+  // })
+
+  return C
 }
--- main.go
+++ main.go
@@ -1,43 +1,66 @@
 package main
 
-import "github.com/gizak/termui"
+import (
+  "./config"
+
+  ui "github.com/gizak/termui"
+  "fmt"
+
+  // "encoding/json"
+  // "fsnotify"
+)
+
+var headers = []string{
+  "Name",
+  "Symbol",
+  "Price in USD",
+  "Price in BTC",
+  "% Change (1h)",
+  "% Change (24h)",
+}
 
 func main() {
-	err := termui.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termui.Close()
+  c := config.LoadConfiguration("./configs.yaml")
+  fmt.Println(c)
 
-	rows := [][]string{
-		[]string{
-      "Name",
-      "Symbol",
-      "Price (USD)",
-      "Price (BTC)",
-      "% Change (1h)",
-      "% Change (24h)"
-    },
-	}
+  err := ui.Init()
+  if err != nil {
+    panic(err)
+  }
+  defer ui.Close()
 
-	table1 := termui.NewTable()
-	table1.Rows = rows
-	table1.FgColor = termui.ColorWhite
-	table1.BgColor = termui.ColorDefault
-	table1.Y = 0
-	table1.X = 0
-	table1.Width = 62
-	table1.Height = 7
+  rows := [][]string{
+    headers,
+  }
 
-	termui.Render(table1)
+  table1 := ui.NewTable()
+  table1.Rows = rows
+  table1.FgColor = ui.ColorWhite
+  table1.BgColor = ui.ColorDefault
+  table1.Separator = false
+  table1.Analysis()
+  table1.SetSize()
+  table1.Y = 0
+  table1.X = 0
+  // table1.Width = 100
+  table1.Height = 200
 
-	termui.Handle("/sys/kbd/q", func(termui.Event) {
-		termui.StopLoop()
-	})
+  // build
+  ui.Body.AddRows(
+    ui.NewRow(ui.NewCol(12, 0, table1)),
+  )
 
-	termui.Handle("/timer/1m", func(e ui.Event) {
+  ui.Body.Align()
+
+  ui.Render(ui.Body)
+
+  ui.Handle("/sys/kbd/q", func(ui.Event) {
+    ui.StopLoop()
+  })
+
+  ui.Handle("/timer/1m", func(e ui.Event) {
     // Refresh drawing
-	})
+  })
 
-	termui.Loop()
+  ui.Loop()
 }
